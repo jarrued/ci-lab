593875dd2bad5e693717c0d6131bfb8c13577b92
jarrued <jgarrued@gmail.com>
2022-10-19 11:31:14 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -16,7 +16,11 @@ public class App {
     if (a == b || a == c || b == c) {
       return "es un triangulo isoceles";
     }
-    if(a !=b && b!=c && a!=c){
+    if((a*a + b*b) == c*c){
+      return "es un triangulo pitagorico";
+    }
+    
+    if(a != b && b != c && a != c){
       return "es un triangulo escaleno";
     }
     return "error no es un triangulo";
